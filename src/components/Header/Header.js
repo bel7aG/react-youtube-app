fca6f5e04b82fe0165c912d40da78fe319c92105
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-08-10 17:59:13 +0200

    header init

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => (
   <header className="header">
     <h1 className="heading-primary">
-      <span className="heading-primary-main">Youtube</span>
-      <span className="heading-primary-subtitle">using ReactJS</span>
+      <span className="heading-primary__main">Youtube</span>
+      <span className="heading-primary__subtitle">ReactJS</span>
     </h1>
   </header>
 );
